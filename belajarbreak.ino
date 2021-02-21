f83cb27fe6e89ce001b3bfe431260c4d299ed953
justadnan <adnanmuh0@gmail.com>
2021-02-21 22:23:08 +0700

    Create belajarbreak.ino

---
+++ belajarbreak.ino
@@ -0,0 +1,50 @@
+int pin=2;
+char current;
+int button;
+char prev;
+int a=250;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(pin,INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i=0; i<255; i++){
+    button=digitalRead(pin);
+    nilai();
+    Serial.println(a);
+    if(current==prev){
+      Serial.println("NILAI SAMA");
+      break;
+    }
+    else{
+      Serial.println("Different State");
+      konfersi();
+      prev=current;
+    }
+  }
+}
+void nilai(){
+  if (button==1){
+    current='A';
+    Serial.println("HIGH");
+    Serial.println(current);
+  }
+  else if(button==0){
+    current='B';
+    Serial.println("LOW");
+    Serial.println(current);
+  }
+  else{}
+}
+void konfersi(){
+  if(current=='A'){
+   a++; 
+  }
+  else if(current=='B'){
+   a--;
+  }
+  else{}
+}
